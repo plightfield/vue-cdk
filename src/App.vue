49c76aff658f3f57255ca1acbf06358484539343
zcating <zcating@qq.com>
2020-09-06 22:50:45 +0800

    init overlay

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,22 @@
-<template></template>
-
 <script lang="ts">
+import { reactive, ref, watchEffect } from "vue";
+
 export default {
-  name: "App",
-};
+  setup() {
+    const count = ref(0);
+    watchEffect(() => {
+        console.log(count.value);
+    });
+
+    return {
+      count
+    }
+  }
+}
+
 </script>
+
+<template>
+  <div>{{ count }}</div>
+</template>
+
