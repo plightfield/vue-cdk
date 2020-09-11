f7c9c15287c1724d8eb84f4a361a29da375d3e73
杜帅(dushuai) <dushuai@qdingnet.cn>
2020-09-11 19:07:31 +0800

    init

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
+  preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  roots: ["<rootDir>/test"],
+  testRegex: "test/(.+)\\.test\\.(jsx?|tsx?|vue?)$",
   transform: {
-    '^.+\\.vue$': 'vue-jest'
-  }
-}
+    "^.+\\.vue$": "vue-jest",
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "vue"],
+};
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { defineComponent } from "vue";
+
+const App = defineComponent(function () {
+  return () => <div>this is cdk</div>;
+});
+App.displayName = "App";
+
+export default App;
