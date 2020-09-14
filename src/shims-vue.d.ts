17e2a188c708af5103dc6e4ee76f000873a00c82
杜帅(dushuai) <dushuai@qdingnet.cn>
2020-09-14 15:38:39 +0800

    remove good listener and add rxjs

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,12 +3,3 @@ declare module "*.vue" {
   const component: ReturnType<typeof defineComponent>;
   export default component;
 }
-
-declare module "good-listener" {
-  const listen: (
-    el: string | Element,
-    event: string,
-    cb: (e: any) => void
-  ) => { destory: () => void };
-  export default listen;
-}
