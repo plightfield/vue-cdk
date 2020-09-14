a677ccea967c8546427982db724c9bd9018c3d4e
杜帅(dushuai) <dushuai@qdingnet.cn>
2020-09-14 15:34:41 +0800

    add good listener

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -1,10 +1,14 @@
-declare module '*.vue' {
-  import { defineComponent } from 'vue'
-  const component: ReturnType<typeof defineComponent>
-  export default component
+declare module "*.vue" {
+  import { defineComponent } from "vue";
+  const component: ReturnType<typeof defineComponent>;
+  export default component;
 }
 
-declare module 'good-listener' {
-  const listen: any
-  export default listen
+declare module "good-listener" {
+  const listen: (
+    el: string | Element,
+    event: string,
+    cb: (e: any) => void
+  ) => { destory: () => void };
+  export default listen;
 }
