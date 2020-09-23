b0eeee52a7bc582706a3838e45df0deaa30284cb
杜帅(dushuai) <dushuai@qdingnet.cn>
2020-09-23 14:21:53 +0800

    delete animation syp

--- lib/Test.tsx
+++ lib/Test.tsx
@@ -3,7 +3,6 @@ import BIDI from "./global/bidirection.spec";
 import BP from "./global/breakpoint.spec";
 import PLAT from "./global/platform.spec";
 import CB from "./global/clipboard.spec";
-import AS from "./global/animationSpy.spec";
 
 export default defineComponent({
   name: "test",
@@ -18,8 +17,6 @@ export default defineComponent({
         <PLAT />
         <h2>clipboard</h2>
         <CB />
-        <h2>animation spy</h2>
-        <AS />
       </>
     );
   },
