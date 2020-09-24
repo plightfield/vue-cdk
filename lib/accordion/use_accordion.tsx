986197b35da924381d5c26f9fa0c9ca808cc4b7c
zcating <zhuchenxi@ut.cn>
2020-09-24 14:52:57 +0800

    add easier way to create accordion

--- lib/accordion/use_accordion.tsx
+++ lib/accordion/use_accordion.tsx
@@ -1,90 +1,90 @@
-import { CdkAny } from 'lib/types';
-import { VNodeChild, reactive } from 'vue';
-import { AccordionDispatcher, CdkAccordionContainer } from './accordion_container';
-import { AccordionItemSlotProps, CdkAccordionItem } from './accordion_item';
-import { AccordionItemBuilder, AccordionItemSlotBuilder, AccordionSlotProps } from './accordion_type';
+// import { CdkAny } from 'lib/types';
+// import { VNodeChild, reactive } from 'vue';
+// import { AccordionDispatcher, CdkAccordionContainer } from './accordion_container';
+// import { AccordionItemSlotProps, CdkAccordionItem } from './accordion_item';
+// import { AccordionItemBuilder, AccordionItemSlotBuilder, AccordionSlotProps } from './accordion_type';
 
-export const useAccordionItem = (
-  dispatcher: AccordionDispatcher,
-  builder: AccordionItemSlotBuilder,
-) => {
-  const slots = {
-    default: (state: AccordionItemSlotProps) => builder(state),
-  };
-  return (
-    <CdkAccordionItem
-      dispatcher={dispatcher}
-      v-slots={slots}
-    ></CdkAccordionItem>
-  );
-}
+// export const useAccordionItem = (
+//   dispatcher: AccordionDispatcher,
+//   builder: AccordionItemSlotBuilder,
+// ) => {
+//   const slots = {
+//     default: (state: AccordionItemSlotProps) => builder(state),
+//   };
+//   return (
+//     <CdkAccordionItem
+//       dispatcher={dispatcher}
+//       v-slots={slots}
+//     ></CdkAccordionItem>
+//   );
+// }
 
-/**
- * @description
- * assembly accordion component in an easy way.
- * @date 2020-09-24
- * @export
- * @function useAccordion
- */
-export const useAccordion = (builders: AccordionItemSlotBuilder[]) => {
-  const state = reactive({
-    multi: false,
-    expanded: false
-  });
+// /**
+//  * @description
+//  * assembly accordion component in an easy way.
+//  * @date 2020-09-24
+//  * @export
+//  * @function useAccordion
+//  */
+// export const useAccordion = (builders: AccordionItemSlotBuilder[]) => {
+//   const state = reactive({
+//     multi: false,
+//     expanded: false
+//   });
 
-  const accordionSlots = {
-    default: ({ dispatcher }: AccordionSlotProps) => {
-      return builders.map((builder) => {
-        return useAccordionItem(dispatcher, builder);
-      });
-    }
-  };
+//   const accordionSlots = {
+//     default: ({ dispatcher }: AccordionSlotProps) => {
+//       return builders.map((builder) => {
+//         return useAccordionItem(dispatcher, builder);
+//       });
+//     }
+//   };
 
-  const element = () => (
-    <CdkAccordionContainer
-      expanded={state.expanded}
-      multi={state.multi}
-      v-slots={accordionSlots}
-    ></CdkAccordionContainer>
-  )
+//   const element = () => (
+//     <CdkAccordionContainer
+//       expanded={state.expanded}
+//       multi={state.multi}
+//       v-slots={accordionSlots}
+//     ></CdkAccordionContainer>
+//   )
 
-  return {
-    state,
-    element
-  }
-};
+//   return {
+//     state,
+//     element
+//   }
+// };
 
-/**
- * @description
- * assembly accordion component in an easy way.
- * @date 2020-09-24
- * @export
- * @function useCustomAccordion
- */
-export const useCustomAccordion = (builders: AccordionItemBuilder[]) => {
-  const state = reactive({
-    multi: false,
-    expanded: false
-  });
+// /**
+//  * @description
+//  * assembly accordion component in an easy way.
+//  * @date 2020-09-24
+//  * @export
+//  * @function useCustomAccordion
+//  */
+// export const useCustomAccordion = (builders: AccordionItemBuilder[]) => {
+//   const state = reactive({
+//     multi: false,
+//     expanded: false
+//   });
 
-  const accordionSlots = {
-    default: ({ dispatcher }: AccordionSlotProps) => {
-      return builders.map((builder) => {
-        return builder(dispatcher);
-      });
-    }
-  };
+//   const accordionSlots = {
+//     default: ({ dispatcher }: AccordionSlotProps) => {
+//       return builders.map((builder) => {
+//         return builder(dispatcher);
+//       });
+//     }
+//   };
 
-  const element = () => (
-    <CdkAccordionContainer
-      expanded={state.expanded}
-      multi={state.multi}
-      v-slots={accordionSlots}
-    ></CdkAccordionContainer>
-  )
+//   const element = () => (
+//     <CdkAccordionContainer
+//       expanded={state.expanded}
+//       multi={state.multi}
+//       v-slots={accordionSlots}
+//     ></CdkAccordionContainer>
+//   )
 
-  return {
-    state,
-    element
-  }
-}
+//   return {
+//     state,
+//     element
+//   }
+// }
